84b75a00d856a7112a699123fc44a118aa373ca5
muhammadfarooq1058 <muhammadfarooq1058@gmail.com>
2024-12-14 19:25:40 +0500

    01CustomReact

--- 01CustomReact/react.js
+++ 01CustomReact/react.js
@@ -1,10 +1,4 @@
-# O1CustomReact
->**index.html**
->> It is for the ReactElement to render.It conatins basically a signle '''Html <div id="root"></div> '''
-
->**script.js**
->> It is for core js.It gives the concept of react how create element behind the scene.I have created a custom **ReactElement** and render it in **index.html** 
-'''javaScript function Render(reactElement,conatianer){
+function Render(reactElement,conatianer){
     let element=document.createElement(reactElement.type);
     element.innerHTML=reactElement.child;
     for (const key in reactElement.props ){
@@ -24,4 +18,3 @@ let reactElement={
 }
 let root=document.querySelector("#root");
 Render(reactElement,root);
-'''
